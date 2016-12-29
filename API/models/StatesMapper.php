e85e93c87f069e76465aa5dac60f9081aad51abf
paulchu756 <lordbabypanda@gmail.com>
2016-12-29 17:25:34 +0000

    delete comment

--- API/models/StatesMapper.php
+++ API/models/StatesMapper.php
@@ -2,33 +2,6 @@
 
 class API_Model_StatesMapper
 {
-    /*
-    protected $_dbTable;
-    
-    public function setDbTable($dbTable)
-    {
-        if(is_string($dbTable))
-        {
-            $dbTable = new $dbTable();
-        }
-        if(!$dbTable instanceof Zend_Db_Table_Abstract)
-        {
-            throw new Exception('Invalid table data gateway provided');
-        }
-        $this->_dbTable = $dbTable;
-        return $this;
-    }
-    
-    public function getDbTable()
-    {
-        if( null === $this->_dbTable)
-        {
-            $this->setDbTable('API_Model_DbTable_States');
-        }
-        return $this->_dbTable;
-    }
-    */
-
     public function save(API_Model_States $states)
     {
         $data = array(
--- API/models/VisitsMapper.php
+++ API/models/VisitsMapper.php
@@ -2,33 +2,6 @@
 
 class API_Model_VisitsMapper
 {
-    /*
-    protected $_dbTable;
-    
-    public function setDbTable($dbTable)
-    {
-        if(is_string($dbTable))
-        {
-            $dbTable = new $dbTable();
-        }
-        if(!$dbTable instanceof Zend_Db_Table_Abstract)
-        {
-            throw new Exception('Invalid table data gateway provided');
-        }
-        $this->_dbTable = $dbTable;
-        return $this;
-    }
-    
-    public function getDbTable()
-    {
-        if( null === $this->_dbTable)
-        {
-            $this->setDbTable('API_Model_DbTable_Visits');
-        }
-        return $this->_dbTable;
-    }
-    */
-
     public function save(API_Model_Visits $visits)
     {
         $data = array(
